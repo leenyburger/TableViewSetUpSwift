9848e688e17ceeab69dc01a32a04520d9aa43515
Schnettler Family <billpaystation@Schnettlers-iMac.local>
2014-10-17 10:23:38 -0400

    Initial Commit

---
+++ TableViews/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TableViews
+//
+//  Created by Schnettler Family on 10/17/14.
+//  Copyright (c) 2014 BitmappedDesigns. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
